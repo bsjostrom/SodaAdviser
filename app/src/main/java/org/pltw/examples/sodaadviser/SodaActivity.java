0bf4355c4a65577d3ebab9eb13392828fc3dc2a5
Bailee Sjostrom <20sjobai@student.mcpsmt.org>
2017-11-28 10:28:53 -0700

    11/28/2017

--- app/src/main/java/org/pltw/examples/sodaadviser/SodaActivity.java
+++ app/src/main/java/org/pltw/examples/sodaadviser/SodaActivity.java
@@ -2,14 +2,43 @@ package org.pltw.examples.sodaadviser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SodaActivity extends AppCompatActivity {
 private Button findSodaButton;
+private TextView brandsTextView;
+private Spinner colorSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soda);
         findSodaButton = (Button) findViewById(R.id.findSodaButton);
+        brandsTextView = (TextView) findViewById(R.id.brandsTextView);
+        colorSpinner = (Spinner) findViewById(R.id.colorSpinner); colorSpinner.getSelectedItem();
+
+        findSodaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((String.valueOf(colorSpinner.getSelectedItem()).equals("Lemon-Lime"))) {
+                    brandsTextView.setText("Sprite");
+                }
+                else if ((String.valueOf(colorSpinner.getSelectedItem()).equals("Fruit"))) {
+                    brandsTextView.setText("Welches Grape Soda\n" + " Crush Orange Soda");
+                }
+                else if ((String.valueOf(colorSpinner.getSelectedItem()).equals("Cola"))) {
+                    brandsTextView.setText("Coca-cola");
+                }
+                else if ((String.valueOf(colorSpinner.getSelectedItem()).equals("Caffeinated Citrus"))) {
+                    brandsTextView.setText("Mountain Dew\n" + " Mellow Yellow");
+                }
+
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/org/pltw/examples/sodaadviser/SodaExpert.java
@@ -0,0 +1,8 @@
+package org.pltw.examples.sodaadviser;
+
+/**
+ * Created by 20sjobai on 11/28/2017.
+ */
+
+public class SodaExpert {
+}
